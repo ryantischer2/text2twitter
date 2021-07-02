7f33a25aac60b8e5006c4e1c9f749b3936f4fdeb
EC2 Default User <ec2-user@ip-172-31-8-241.us-east-2.compute.internal>
2021-07-02 18:41:32 +0000

    init

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#text2twitter
+
+import random
+
+readFromfile = '/home/ec2-user/environment/text2twitter/readline'
+
+
+lines = open(readFromfile).read().splitlines()
+myline =random.choice(lines)
+print(myline)
